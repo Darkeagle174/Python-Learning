1fe70db46cde178f086d8fdaca3c09a763292fbd
Darkeagle174 <railfannation010704@gmail.com>
2025-07-07 18:08:08 +0530

    Completed Comments

--- Python Concepts/comments.py
+++ Python Concepts/comments.py
@@ -1,1 +1,8 @@
 #this is a comment 
+# Python supports single-line comments using the hash symbol (#)
+# Python ignors everything after the hash symbol on that line
+# You can also use triple quotes for multi-line comments
+"""
+This is a multi-line comment
+It can span multiple lines
+"""
