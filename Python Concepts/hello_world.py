8c4b615855e97b69c1950939bbb3038b7520cd9c
Darkeagle174 <railfannation010704@gmail.com>
2025-07-07 17:34:17 +0530

    Python Learning Begins

---
+++ Python Concepts/hello_world.py
@@ -0,0 +1,5 @@
+#print function prints the value inside the brackets
+print("hello world")
+
+import sys
+print(sys.version)  # prints the version of Python being used
---
+++ Python Concepts/indentation.py
@@ -0,0 +1,4 @@
+# Identation refers to the spaces or tabs at the beginning of a line of code.
+# In Python, indentation is used to define the scope of loops, functions, and classes.
+if 5 > 2:
+    print("Five is greater than two!") # This line is indented, so it belongs to the if block
