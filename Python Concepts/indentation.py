ec1f02783feaf95ab2067a75c3cbc931183e9373
Darkeagle174 <railfannation010704@gmail.com>
2025-07-07 17:58:00 +0530

    Learnt Identation Error and began Comments

--- Python Concepts/indentation.py
+++ Python Concepts/indentation.py
@@ -5,3 +5,13 @@ if 5 > 2:
     
 if 5 > 2:
 print("Five is greater than two!") # This line is not indented, so it does not belong to the if block
+
+if 5 > 2:
+    print("Five is greater than two!") # This line is indented, so it belongs to the if block
+    print("This is also part of the if block") # This line is also indented, so it belongs to the if block
+    #same no. of space should be given for the lines inside the same block
+print("This line is outside the if block") # This line is not indented, so it does not belong to the if block
+
+if ( 5 > 2):
+    print("Five is greater than two!") # This line is indented, so it belongs to the if block
+        print("This is also part of the if block") # This line is indented with more spaces, so it does not belong to the if block it causes an IndentationError
